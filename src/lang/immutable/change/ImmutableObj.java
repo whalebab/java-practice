81822a560df7a3d6fa00832ba1b72f2f293b32ed
whalebab <juncoder94@gmail.com>
2024-03-29 23:07:25 +0900

    불변객체 개념정리 및 문제풀이

---
+++ src/lang/immutable/change/ImmutableObj.java
@@ -0,0 +1,20 @@
+package lang.immutable.change;
+
+public class ImmutableObj {
+
+    private final int value;
+
+    public ImmutableObj(int value) {
+        this.value = value;
+    }
+
+    public ImmutableObj add(int addValue) {
+        return new ImmutableObj(value + addValue);
+        // int result = value + addValue;
+        // return new ImmutableObj(result);
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
