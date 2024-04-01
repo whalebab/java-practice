53a6f1bcaaaf46c9e5a20b35d99b58e28c7ea8ff
whalebab <juncoder94@gmail.com>
2024-04-02 00:39:52 +0900

    열거형 - ENUM 개념정리

---
+++ src/enumeration/ex2/ClassGrade.java
@@ -0,0 +1,12 @@
+package enumeration.ex2;
+
+// 회원 등급을 다루는 클래스
+// ※ 타입 안전 열거형 패턴
+public class ClassGrade {
+    public static final ClassGrade BASIC = new ClassGrade(); // x001
+    public static final ClassGrade GOLD = new ClassGrade(); // x002
+    public static final ClassGrade DIAMOND = new ClassGrade(); // x003
+
+    // private 생성자 추가
+    private ClassGrade() {}
+}
---
+++ src/enumeration/ref3/DiscountService.java
@@ -0,0 +1,6 @@
+package enumeration.ref3;
+public class DiscountService {
+    public int discount(Grade grade, int price) {
+        return grade.discount(price);
+    }
+}
