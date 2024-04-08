3c14d336dfd61dbba1f60131a9fe4eb9579c9027
whalebab <juncoder94@gmail.com>
2024-04-08 23:38:09 +0900

    중첩 클래스, 내부 클래스 1 개념정리

---
+++ src/nested/inner/InnerOuterMain.java
@@ -0,0 +1,11 @@
+package nested.inner;
+
+public class InnerOuterMain {
+    public static void main(String[] args) {
+        InnerOuter outer = new InnerOuter();
+        InnerOuter.Inner inner = outer.new Inner(); // new 바깥클래스의 인스턴스 참조.내부클래스()
+        inner.print();
+
+        System.out.println("innerClass = " + inner.getClass());
+    }
+}
