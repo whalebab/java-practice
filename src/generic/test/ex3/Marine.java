6f50db3ff1b7a278d7b8089feab6106b62178876
whalebab <juncoder94@gmail.com>
2024-06-05 01:04:11 +0900

    제네릭 - Generic2 문제풀이

---
+++ src/generic/test/ex3/Marine.java
@@ -0,0 +1,7 @@
+package generic.test.ex3;
+
+public class Marine extends BioUnit {
+    public Marine(String name, int hp) {
+        super(name, hp);
+    }
+}
---
+++ src/generic/test/ex3/Shuttle.java
@@ -0,0 +1,18 @@
+package generic.test.ex3;
+
+public class Shuttle<T extends BioUnit> {
+
+    private T unit;
+
+    public void in(T t) {
+        unit = t;
+    }
+
+    public T out() {
+        return unit;
+    }
+
+    public void showInfo() {
+        System.out.println("이름: " + unit.getName() + ", " + "HP: " + unit.getHp());
+    }
+}
